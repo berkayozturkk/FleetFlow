aa9789cf43b16a4237ef2bf726ad7dbf39576955
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-12-03 00:27:41 +0300

    Repository

---
+++ Core/FleetFlow.Application/Abstractions/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace FleetFlow.Application.Abstractions;
+
+public interface IRepository<TEntity, TId> where TEntity : class 
+{
+    Task<List<TEntity>> GetAllAsync();
+    Task<TEntity> GetByIdAsync(TId id);
+    Task CreateAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+}
---
+++ Instrafracture/FleetFlow.Persistance/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using FleetFlow.Application.Abstractions;
+using FleetFlow.Domain.Abstraction;
+using FleetFlow.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetFlow.Persistance.Repositories;
+
+public class Repository<TEntity, TId> : IRepository<TEntity, TId> 
+    where TEntity : Entity<TId>
+{
+    private readonly BaseDbContext _dbContext;
+
+    public Repository(BaseDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task CreateAsync(TEntity entity)
+    {
+        entity.CreatedDate = DateTime.Now;
+        await _dbContext.Set<TEntity>().AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<List<TEntity>> GetAllAsync()
+    {
+        return await _dbContext.Set<TEntity>().ToListAsync();
+    }
+
+    public async Task<TEntity> GetByIdAsync(TId id)
+    {
+        return await _dbContext.Set<TEntity>().FindAsync(id);
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        entity.UpdatedDate = DateTime.Now;
+        _dbContext.Set<TEntity>().Update(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+}
